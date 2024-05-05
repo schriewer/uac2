4d93a8b082c437e31d83e88365fa5064ffee625b
schriewer <52544452+schriewer@users.noreply.github.com>
2024-05-05 22:36:44 +0200

    Shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    
-    <main>Root page</main>
+    <main>
+      Root page
+      <Button variant="destructive">Click me</Button>
+      <Badge variant="secondary">Badge</Badge>
+    </main>
   );
 }
